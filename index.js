0561669c9710491c55b2c0389b317b368b2a1117
zefe <intern.zeferino@relativity6.com>
2019-03-01 20:49:21 -0600

    Serving static files in Express

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use('/files', express.static('public'))
+
 app.get('/', (req, res) => {
     console.log(`http:// ${req.hostname}:${port}`)
     res.send('Hello World!')
+
+    console.log(res.statusCode)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
